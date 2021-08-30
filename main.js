b534f27cc578df1d95a2c4b05219233e52403bd0
pedrofigueroa7 <69482282+pedrofigueroa7@users.noreply.github.com>
2021-08-30 15:27:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu-btn').addEventListener('click',() =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+
+
+});
+
+ScrollReveal().reveal('.showcase');
+
+ScrollReveal().reveal('.new-cards',{delay:500});
+
+ScrollReveal().reveal('.cards-banner-one',{delay:500});
+
+ScrollReveal().reveal('.cards-banners-two',{delay:500});
